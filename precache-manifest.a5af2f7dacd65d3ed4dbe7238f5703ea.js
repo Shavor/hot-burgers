3e8b7935b98cb05384a4f976f68a7e42af57159a
Alexander Brodnikov <121760509+Shavor@users.noreply.github.com>
2024-06-29 17:52:24 +0300

    Updates

---
+++ precache-manifest.a5af2f7dacd65d3ed4dbe7238f5703ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e66f0ca7f1ef85af84233a5d2b944c",
+    "url": "/hot-burgers/index.html"
+  },
+  {
+    "revision": "12a7a86e5e998d3044c8",
+    "url": "/hot-burgers/static/css/main.334a49aa.chunk.css"
+  },
+  {
+    "revision": "c915727901f9d66459ea",
+    "url": "/hot-burgers/static/js/2.8a12a16a.chunk.js"
+  },
+  {
+    "revision": "12a7a86e5e998d3044c8",
+    "url": "/hot-burgers/static/js/main.21bb707e.chunk.js"
+  },
+  {
+    "revision": "a5a0e5b337eff91b963e",
+    "url": "/hot-burgers/static/js/runtime-main.eb76173a.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/hot-burgers/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/hot-burgers/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/hot-burgers/static/media/up.551b5c6b.svg"
+  }
+]);
